e6fe0a5442b057423668489d11d906c6130ef7cd
leaderg <leader.g@gmail.com>
2019-06-13 21:45:44 +0000

    Finished order details implementation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,4 +38,14 @@ class ApplicationController < ActionController::Base
     redirect_to '/login' unless current_user
   end
 
+   def enhanced_order
+    @enhanced_order ||= @order.line_items.all.map {|item| { product: item.product, quantity: item.quantity } }
+  end
+  helper_method :enhanced_order
+
+  def order_subtotal_cents
+    enhanced_order.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
+  end
+  helper_method :order_subtotal_cents
+
 end
